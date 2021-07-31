6112421ea8dd81516556756159d5e1480da3cf2b
seodong4378 <seodong4378@naver.com>
2021-07-31 20:56:22 +0900

    graphql yoga finish

--- graphql/db.js
+++ graphql/db.js
@@ -1,61 +1,28 @@
-export let movies = [
-    {
-        id: 0,
-        name: "Star Wars-Last jadai",
-        score: 7.1
-    },
-    {
-        id: 1,
-        name: "Dark night",
-        score: 9.0
-    },
-    {
-        id: 2,
-        name: "Soul",
-        score: 9.9
-    },
-    {
-        id: 3,
-        name: "007-Skyfall",
-        score: 8.7,
-    },
-    {
-        id: 4,
-        name: "Misson-impossible",
-        score: 8.9,
-    },
-    {
-        id: 5,
-        name: "Parasite",
-        score: 8.3,
+import axios from "axios"
+import { response } from "express";
+
+let MOIVE_URL = "https://yts.mx/api/v2/list_movies.json?";
+
+export const getMovie = async (limit, rating) => {
+    let REQ_URL = MOIVE_URL;
+
+    if (limit > 0) {
+        REQ_URL += `limit=${limit}`
     }
-]
 
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-}
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: movies.length,
-        name,
-        score
-    };
-
-    movies.push(newMovie);
-    return newMovie;
-}
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
+    if (rating > 0) {
+        REQ_URL += `&minimum_rating=${rating}`
     }
+
+    const {
+        data: {
+            data: {movies}
+        }
+    } = await axios.get(REQ_URL);
+    
+    console.log(`URL : ${REQ_URL}`);
+
+    return movies;
 }
 
 
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,8 @@
-import {movies, getById, addMovie, deleteMovie} from "./db"
+import {getMovie} from "./db"
 
 const resolvers = {
     Query:{
-        movies: () => movies,
-        movie: (_, {id}) => getById(id)
-    },
-    Mutation:{
-        addMovie: (_, {name, score}) => addMovie(name, score),
-        deleteMovie: (_, {id}) => deleteMovie(id)
+        movies: (_,{limit, rating}) => getMovie(limit, rating)
     }
 };
 
